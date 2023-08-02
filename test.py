edeee2fa8735bd6e0cac9c510b31c0de677a280d
TaplierShiru <danil.gribanov01@gmail.com>
2023-08-02 11:52:41 +0400

    fix test.py

--- test.py
+++ test.py
@@ -16,34 +16,37 @@ def as_mesh(scene_or_mesh):
                 tuple(trimesh.Trimesh(vertices=g.vertices, faces=g.faces)
                     for g in scene_or_mesh.geometry.values()))
     else:
-        assert(isinstance(mesh, trimesh.Trimesh))
+        assert(isinstance(scene_or_mesh, trimesh.Trimesh))
         mesh = scene_or_mesh
     return mesh
 
 # rest of code
-mesh_1: trimesh.Trimesh = trimesh.load("lfd/examples/cup1.obj")
-mesh_2: trimesh.Trimesh = as_mesh(trimesh.load("lfd/examples/airplane.obj"))
+mesh_cap1: trimesh.Trimesh = trimesh.load("lfd/examples/cup1.obj")
+mesh_airplane: trimesh.Trimesh = as_mesh(trimesh.load("lfd/examples/airplane.obj"))
+mesh_cap2: trimesh.Trimesh = as_mesh(trimesh.load("lfd/examples/cup2.obj"))
+mesh_3dcafe: trimesh.Trimesh = as_mesh(trimesh.load("lfd/examples/3dcafe_a-10.obj"))
 
 lfd_value: float = LightFieldDistance(verbose=True).get_distance(
-    mesh_1.vertices, mesh_1.faces,
-    mesh_2.vertices, mesh_2.faces
+    mesh_cap1, mesh_airplane
 )
 print(f'Between cup1 and airplane = {lfd_value}')
 
-mesh_1: trimesh.Trimesh = trimesh.load("lfd/examples/cup1.obj")
-mesh_2: trimesh.Trimesh = trimesh.load("lfd/examples/cup1.obj")
-
 lfd_value: float = LightFieldDistance(verbose=True).get_distance(
-    mesh_1.vertices, mesh_1.faces,
-    mesh_2.vertices, mesh_2.faces
+    mesh_cap1, mesh_cap1
 )
 print(f'Between cup1 and cup1 = {lfd_value}')
 
-mesh_1: trimesh.Trimesh = as_mesh(trimesh.load("lfd/examples/airplane.obj"))
-mesh_2: trimesh.Trimesh = as_mesh(trimesh.load("lfd/examples/airplane.obj"))
-
 lfd_value: float = LightFieldDistance(verbose=True).get_distance(
-    mesh_1.vertices, mesh_1.faces,
-    mesh_2.vertices, mesh_2.faces
+    mesh_airplane, mesh_airplane
 )
 print(f'Between airplane and airplane = {lfd_value}')
+
+lfd_value: float = LightFieldDistance(verbose=True).get_distance(
+    mesh_cap1, mesh_cap2
+)
+print(f'Between cup1 and cup2 = {lfd_value}')
+
+lfd_value: float = LightFieldDistance(verbose=True).get_distance(
+    mesh_airplane, mesh_3dcafe
+)
+print(f'Between airplane and 3dcafe = {lfd_value}')
